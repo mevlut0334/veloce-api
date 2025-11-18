82ff3176ddab535c687d36d617b122b00b92120b
MEVLUT TUNCER <mevluttuncer0334@gmail.com>
2025-11-18 14:49:47 +0300

    user admin güncelleme

---
+++ app/Filament/Resources/Users/Widgets/SubscriptionStatsWidget.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\Users\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class SubscriptionStatsWidget extends StatsOverviewWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ app/Filament/Resources/Users/Widgets/UserStatsWidget.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\Users\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class UserStatsWidget extends StatsOverviewWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            //
+        ];
+    }
+}
